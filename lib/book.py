777fbedef43cf08548aaf98c7b345ae3f3a4c64b
Daniel Morley <morleydan10@gmail.com>
2023-12-14 14:10:13 -0500

    solutions

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,47 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+
+    def __init__(self, title = '', page_count = 0):
+        self.title = title
+        self.page_count = page_count
+
+    @property
+    def title(self):
+        return self._title
+    
+    @title.setter
+    def title(self, title):
+        if isinstance(title, str):
+            self._title = title
+            print(f"Title: {title}")
+        else: 
+            raise TypeError("Title must be a string.")
+    
+
+    @property
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, page_count):
+        if isinstance(page_count, int):
+            self._page_count = page_count
+            print(f"Page Count: {page_count} pages")
+        else:
+            # Normally, you would use raise TypeError("page_count must be an integer")
+            print("page_count must be an integer")
+
+    
+    # Test #3 of book_test.py
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+
+
+book = Book("Plunder", 340)
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,40 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+    
+    @property
+    def brand(self):
+        return self._brand
+    
+    @brand.setter
+    def brand(self, brand):
+        self._brand = brand
+        print(f"Brand: {brand}")
+    
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, size):
+        if isinstance(size, int):
+            self._size = size
+            print(f"Size: {size}")
+        else:
+            print("size must be an integer")
+
+# can add an attribute to a class through a method ⬇️
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+
+
+stan_smith = Shoe("Adidas", 9)
+# condition = Shoe(condition = "New")
+
